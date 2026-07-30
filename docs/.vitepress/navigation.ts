fb0c2d7b549730a3822274a90aec424b7cee5602
象限跃迁QUADR-X <contact@sparkx.zone>
2026-07-30 17:32:00 +0800

    rename help navigation label

--- docs/.vitepress/navigation.ts
+++ docs/.vitepress/navigation.ts
@@ -2,7 +2,7 @@ export const nav = [
   { text: '首页', link: '/' },
   { text: '阅读', link: '/wb-x/' },
   { text: '案例集', link: '/cases/' },
-  { text: '帮你解决', link: '/help/' },
+  { text: '提需求', link: '/help/' },
   { text: '指南', link: '/reading-guide' },
   { text: '交流群', link: '#community', custom: true },
 ] as const
