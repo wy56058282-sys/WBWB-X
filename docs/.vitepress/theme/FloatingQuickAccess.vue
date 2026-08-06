4caf22cbeb87c8cf496db5dbc33c632462241ea4
王益 <wangyi@wangyideMac-mini.local>
2026-08-07 07:31:53 +0800

    悬浮球星火集图标更换为星火集 logo

--- docs/.vitepress/theme/FloatingQuickAccess.vue
+++ docs/.vitepress/theme/FloatingQuickAccess.vue
@@ -164,13 +164,8 @@ onBeforeUnmount(() => {
                 d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"
               />
             </svg>
-            <!-- Spark/star icon -->
-            <svg v-else-if="item.icon === 'spark'" viewBox="0 0 24 24" width="20" height="20">
-              <path
-                fill="currentColor"
-                d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-              />
-            </svg>
+            <!-- SparkX logo -->
+            <img v-else-if="item.icon === 'spark'" :src="withBase('/brand/sparkx-logo.svg')" alt="星火集" width="20" height="20" />
             <!-- QR code icon -->
             <svg v-else-if="item.icon === 'qrcode'" viewBox="0 0 24 24" width="20" height="20">
               <path
